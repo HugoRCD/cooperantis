d32482beb860a2d5d48038ef187be003dd8e0872
HugoRCD <hugo.richard@epitech.eu>
2023-01-31 23:20:06 +0100

    Add some traduction

--- pages/home.vue
+++ pages/home.vue
@@ -14,13 +14,13 @@ definePageMeta({
       class="mx-auto max-w-7xl py-16 px-6 text-center sm:py-24 lg:px-8 lg:py-48"
     >
       <h1 class="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl">
-        Cooperantis is coming soon
+        {{ $t("pages.home.hero") }}
       </h1>
       <p class="mt-2 text-lg font-medium text-muted">
-        We are working hard to get this ready for you.
+        {{ $t("pages.home.description") }}
       </p>
       <div class="mt-6 flex justify-center">
-        <NuxtLink to="/" class="btn-primary w-1/6"> Contact </NuxtLink>
+        <NuxtLink to="/contact" class="btn-primary w-1/6"> Contact </NuxtLink>
       </div>
     </div>
   </main>
